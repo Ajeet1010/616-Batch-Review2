4660d1ee5ec09142fa8dc877a1ee6263ca783ec0
ajeet1010 <ajeetpal101999@gmail.com>
2022-11-21 09:07:24 +0530

    [Ajeet]-array

--- biggerno.sh
+++ biggerno.sh
@@ -1,11 +1,15 @@
 #!/bin/bash  
 
-read num1
-read num2
+read -p "Enter first no:  " num1
+read -p "Enter second no: " num2
 
-if[ $num1 -ge $num2 ];
+if[ $num1 -gt $num2 ];
+then
 	echo "bigger no is $num1";
-else
+elif[$num1 -lt $num2]
+then
 	echo "bigger no is $num2";
+else
+	echo "Both are equal"
 fi
 
